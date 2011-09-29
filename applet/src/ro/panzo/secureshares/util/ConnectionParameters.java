265e3b83a9ec7a97e683edddf51981c9cfa465a1
cticu <cticu>
2011-09-29 11:48:41 +0000

    no message

---
+++ applet/src/ro/panzo/secureshares/util/ConnectionParameters.java
@@ -0,0 +1,31 @@
+package ro.panzo.secureshares.util;
+
+public class ConnectionParameters {
+    private String host;
+    private int port;
+    private String user;
+    private String password;
+
+    public ConnectionParameters(String host, int port, String user, String password) {
+        this.host = host;
+        this.port = port;
+        this.user = user;
+        this.password = password;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
