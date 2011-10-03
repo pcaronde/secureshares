80b18031a86d763ef52a4521a19589e6411959dc
cticu <cticu>
2011-10-03 09:20:01 +0000

    SS-10: get sftp host, user, pass, repository name from properties file

--- javamodule/src/ro/panzo/secureshares/servlet/AppletServlet.java
+++ javamodule/src/ro/panzo/secureshares/servlet/AppletServlet.java
@@ -1,6 +1,7 @@
 package ro.panzo.secureshares.servlet;
 
 import org.apache.log4j.Logger;
+import ro.panzo.secureshares.util.Util;
 
 import javax.crypto.Cipher;
 import javax.crypto.CipherOutputStream;
@@ -24,24 +25,22 @@ public class AppletServlet extends HttpServlet {
                 (byte)0xc7, (byte)0x73, (byte)0x21, (byte)0x8c,
                 (byte)0x7e, (byte)0xc8, (byte)0xee, (byte)0x99
             };
-    private final int count = 20;
-    private int port = 22;
-    //private String host = "212.223.119.68";
-    //private String user = "secureftp";
-    //private String password = "SecureFTP123!";
-
-    private String host = "localhost";
-    private String user = "cticu";
-    private String password = "panzo123";
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         try{
+            String host = Util.getInstance().getEnviromentValue("SFTP_HOST");
+            int port = Integer.parseInt(Util.getInstance().getEnviromentValue("SFTP_PORT"));
+            String user = Util.getInstance().getEnviromentValue("SFTP_USER");
+            String password = Util.getInstance().getEnviromentValue("SFTP_PASSWORD");
+            String repository = Util.getInstance().getEnviromentValue("SFTP_REPOSITORY");
+
             BufferedReader br = new BufferedReader(new InputStreamReader(request.getInputStream()));
             String encPassword = br.readLine();
             SecretKeyFactory factory = SecretKeyFactory.getInstance("PBEWithMD5AndDES");
             Key key = factory.generateSecret(new PBEKeySpec(encPassword.toCharArray()));
             Cipher cipher = Cipher.getInstance("PBEWithMD5AndDES");
+            int count = 20;
             PBEParameterSpec pbeParamSpec = new PBEParameterSpec(salt, count);
             cipher.init(Cipher.ENCRYPT_MODE, key, pbeParamSpec);
             CipherOutputStream out = new CipherOutputStream(response.getOutputStream(), cipher);
@@ -50,6 +49,7 @@ public class AppletServlet extends HttpServlet {
             dout.writeInt(port);
             dout.writeUTF(user);
             dout.writeUTF(password);
+            dout.writeUTF(repository);
             dout.close();
             out.close();
         } catch (Exception ex){
