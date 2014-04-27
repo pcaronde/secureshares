f9dcf181120ec7ca661f89ffc0be2323f9c21a30
Peter Caron <pcaron.secureshares@gmail.com>
2014-04-27 09:04:18 +0200

    gitignore needs to be set properly. Convert to Maven project. pom.xml should be one dir higher.

---
+++ applet/src/ch/ethz/ssh2/channel/ChannelManagerTest.java
@@ -0,0 +1,150 @@
+package ch.ethz.ssh2.channel;
+
+import junit.framework.TestCase;
+
+public class ChannelManagerTest extends TestCase {
+
+    public void testRegisterX11Cookie() throws Exception {
+
+    }
+
+    public void testUnRegisterX11Cookie() throws Exception {
+
+    }
+
+    public void testCheckX11Cookie() throws Exception {
+
+    }
+
+    public void testCloseAllChannels() throws Exception {
+
+    }
+
+    public void testCloseChannel() throws Exception {
+
+    }
+
+    public void testSendEOF() throws Exception {
+
+    }
+
+    public void testSendOpenConfirmation() throws Exception {
+
+    }
+
+    public void testSendData() throws Exception {
+
+    }
+
+    public void testRequestGlobalForward() throws Exception {
+
+    }
+
+    public void testRequestCancelGlobalForward() throws Exception {
+
+    }
+
+    public void testRegisterThread() throws Exception {
+
+    }
+
+    public void testOpenDirectTCPIPChannel() throws Exception {
+
+    }
+
+    public void testOpenSessionChannel() throws Exception {
+
+    }
+
+    public void testRequestPTY() throws Exception {
+
+    }
+
+    public void testRequestX11() throws Exception {
+
+    }
+
+    public void testRequestSubSystem() throws Exception {
+
+    }
+
+    public void testRequestExecCommand() throws Exception {
+
+    }
+
+    public void testRequestShell() throws Exception {
+
+    }
+
+    public void testMsgChannelExtendedData() throws Exception {
+
+    }
+
+    public void testWaitForCondition() throws Exception {
+
+    }
+
+    public void testGetAvailable() throws Exception {
+
+    }
+
+    public void testGetChannelData() throws Exception {
+
+    }
+
+    public void testMsgChannelData() throws Exception {
+
+    }
+
+    public void testMsgChannelWindowAdjust() throws Exception {
+
+    }
+
+    public void testMsgChannelOpen() throws Exception {
+
+    }
+
+    public void testMsgChannelRequest() throws Exception {
+
+    }
+
+    public void testMsgChannelEOF() throws Exception {
+
+    }
+
+    public void testMsgChannelClose() throws Exception {
+
+    }
+
+    public void testMsgChannelSuccess() throws Exception {
+
+    }
+
+    public void testMsgChannelFailure() throws Exception {
+
+    }
+
+    public void testMsgChannelOpenConfirmation() throws Exception {
+
+    }
+
+    public void testMsgChannelOpenFailure() throws Exception {
+
+    }
+
+    public void testMsgGlobalRequest() throws Exception {
+
+    }
+
+    public void testMsgGlobalSuccess() throws Exception {
+
+    }
+
+    public void testMsgGlobalFailure() throws Exception {
+
+    }
+
+    public void testHandleMessage() throws Exception {
+
+    }
+}
