9e68306b73eec5ba09fa4a3e0990dbeaca859d1e
pcaron4de <pcaron_de@yahoo.com>
2015-03-22 15:29:09 +0100

    Download Test stub

---
+++ web/src/test/java/ro/panzo/secureshares/pojo/CompanyTest.java
@@ -0,0 +1,23 @@
+package ro.panzo.secureshares.pojo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CompanyTest {
+
+    @Test
+    public void testGetId() throws Exception {
+
+    }
+
+    @Test
+    public void testGetName() throws Exception {
+
+    }
+
+    @Test
+    public void testGetSubrepositoryName() throws Exception {
+
+    }
+}
---
+++ web/src/test/java/ro/panzo/secureshares/pojo/DownloadTest.java
@@ -0,0 +1,31 @@
+package ro.panzo.secureshares.pojo;
+
+import org.junit.Test;
+
+public class DownloadTest {
+
+    @Test
+    public void testGetId() throws Exception {
+
+    }
+
+    @Test
+    public void testGetFile() throws Exception {
+
+    }
+
+    @Test
+    public void testGetDownloadType() throws Exception {
+
+    }
+
+    @Test
+    public void testGetDate() throws Exception {
+
+    }
+
+    @Test
+    public void testGetCount() throws Exception {
+
+    }
+}
