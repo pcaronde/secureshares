8c7609625b8162b8264bd240983cb4a270f70271
pcaron4de <pcaron_de@yahoo.com>
2015-03-22 16:15:05 +0100

    Company Test edits to non-code

--- web/src/test/java/ro/panzo/secureshares/pojo/CompanyTest.java
+++ web/src/test/java/ro/panzo/secureshares/pojo/CompanyTest.java
@@ -9,22 +9,7 @@ import org.junit.Test;
 ;
 
 public class CompanyTest {
-
-    /**
-     *   @Test(expected = IllegalArgumentException.class)
-    public void testExceptionIsThrown() {
-    MyClass tester = new MyClass();
-    tester.multiply(1000, 5);
-    }
-
-     @Test
-     public void testMultiply() {
-     MyClass tester = new MyClass();
-     assertEquals("10 x 5 must be 50", 50, tester.multiply(10, 5));
-     }
-     * @throws Exception
-     */
-
+    
     @Test
     public void testGetId() throws Exception {
 
