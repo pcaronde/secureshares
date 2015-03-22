f91ce46a2d934132370aea1abfc5246e77e138c8
pcaron4de <pcaron_de@yahoo.com>
2015-03-22 16:09:28 +0100

    Company Test getName

--- web/src/test/java/ro/panzo/secureshares/pojo/CompanyTest.java
+++ web/src/test/java/ro/panzo/secureshares/pojo/CompanyTest.java
@@ -1,10 +1,29 @@
 package ro.panzo.secureshares.pojo;
 
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
-import static org.junit.Assert.*;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.Test;
+;
 
 public class CompanyTest {
+
+    /**
+     *   @Test(expected = IllegalArgumentException.class)
+    public void testExceptionIsThrown() {
+    MyClass tester = new MyClass();
+    tester.multiply(1000, 5);
+    }
+
+     @Test
+     public void testMultiply() {
+     MyClass tester = new MyClass();
+     assertEquals("10 x 5 must be 50", 50, tester.multiply(10, 5));
+     }
+     * @throws Exception
+     */
 
     @Test
     public void testGetId() throws Exception {
@@ -13,7 +32,8 @@ public class CompanyTest {
 
     @Test
     public void testGetName() throws Exception {
-
+        Company tester = new Company(1, "caron", "repo");
+        assertEquals("Name should be caron", "caron", tester.getName());
     }
 
     @Test
