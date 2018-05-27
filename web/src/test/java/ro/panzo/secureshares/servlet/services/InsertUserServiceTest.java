2f4916fe2cf581ce2669db0df3f47c88c11c776d
Peter Caron <pcaron.de@gmail.com>
2018-05-27 14:58:41 +0200

    Added test stubs

--- web/src/test/java/ro/panzo/secureshares/servlet/services/InsertUserServiceTest.java
+++ web/src/test/java/ro/panzo/secureshares/servlet/services/InsertUserServiceTest.java
@@ -1,21 +1,28 @@
 package ro.panzo.secureshares.servlet.services;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
+/**
+ * Created by petercaron on 27.05.18.
+ */
 public class InsertUserServiceTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
 
     @Test
     public void testExecute() throws Exception {
-
-    }
-
-    @Test
-    public void testGetName() throws Exception {
-
-    }
-
-    @Test
-    public void testGetRole() throws Exception {
 
     }
 }
