71cabe31d7da49bd19d95176b440ad8b159e32b8
pcaronde <pcaron_de@yahoo.com>
2018-05-30 18:59:18 +0200

    Changed DeleteUserTest and SendDownloadLink - incomplete

--- web/src/test/java/ro/panzo/secureshares/servlet/services/DeleteUserServiceTest.java
+++ web/src/test/java/ro/panzo/secureshares/servlet/services/DeleteUserServiceTest.java
@@ -1,13 +1,21 @@
 package ro.panzo.secureshares.servlet.services;
 
+import org.junit.Assert;
 import org.junit.Test;
+import ro.panzo.secureshares.db.DBManager;
+import ro.panzo.secureshares.util.ServiceUtil;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.LinkedList;
+import java.util.List;
 
 public class DeleteUserServiceTest {
 
-    @Test
-    public void testExecute() throws Exception {
+    DeleteUserService delUser = new DeleteUserService();
+    HttpServletResponse response;
+    HttpServletRequest request;
 
-    }
 
     @Test
     public void testGetName() throws Exception {
--- web/src/test/java/ro/panzo/secureshares/servlet/services/SendDownloadLinkServiceTest.java
+++ web/src/test/java/ro/panzo/secureshares/servlet/services/SendDownloadLinkServiceTest.java
@@ -1,17 +1,35 @@
 package ro.panzo.secureshares.servlet.services;
 
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
-import org.mockito.MockitoAnnotations;
 import org.mockito.*;
+
+import org.powermock.api.easymock.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.StringWriter;
+
 
 public class SendDownloadLinkServiceTest {
+/**
+@Test
+public void testExecute() throws Exception {
 
+    SendDownloadLinkService service = PowerMock(new SendDownloadLinkService());
+    StringWriter writer = new StringWriter();
+    HttpServletRequest request = Mock(HttpServletRequest);
+    HttpServletResponse response = Mock(HttpServletResponse);
+
+    //this will collect all data written by your service to response
+    when(response.getWriter).thenReturn(writer);
+    HttpServletSession session = mock(HttpServletSession.class);
+    //we program the mocked request
+    when(request.getParameter("fid")).thenReturn("123");
+    //do the same for all necesary request parameters
+    //use same strategy for mocking the session attributes (the "lang") but first
+    when(request.getSession()).thenReturn(session);
+}
+*/
 /**
     @Mock
     private SendDownloadLinkService downloadLinkService;
