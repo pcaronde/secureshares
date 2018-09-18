5afd2841c39db6d5cd73d6d82717f819bf6a338b
Peter Caron <pcaron.de@gmail.com>
2018-09-18 09:57:22 +0200

    Added test

--- web/src/test/java/ro/panzo/secureshares/servlet/ActionFactoryTest.java
+++ web/src/test/java/ro/panzo/secureshares/servlet/ActionFactoryTest.java
@@ -4,23 +4,32 @@ import junit.framework.Assert;
 import junit.framework.TestCase;
 import org.junit.Test;
 
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+
 
 /**
  * Created by petercaron on 06.08.18.
  *
- * This class tests the getInstance invokation in ActionFactory.java
+ * This class tests the getInstance invocation in ActionFactory.java
  */
 public class ActionFactoryTest extends TestCase {
 
+    ActionFactory af = ActionFactory.getInstance();
+
+
     @Test
     public void testGetInstance() throws Exception {
-        ActionFactory af = ActionFactory.getInstance();
+
         Assert.assertNotNull(af);
         Assert.assertSame(af, ActionFactory.getInstance());
     }
 
     @Test
     public void testExecuteService() throws Exception {
-
+        //af.executeService(request, response);
     }
 }
